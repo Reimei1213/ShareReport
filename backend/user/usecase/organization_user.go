21d5027c0787b7aacdb4aee3022840bbc7de4ddc
reimei <34594195+Reimei1213@users.noreply.github.com>
2022-03-11 21:46:44 +0900

    [fix] #28 OrganizationUser用にプロトコルバッファの記述修正＆コード生成

--- backend/user/usecase/organization_user.go
+++ backend/user/usecase/organization_user.go
@@ -3,20 +3,22 @@ package usecase
 import (
 	"context"
 	pb "share-report/proto/user"
+
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 func (s *UserService) GetOrganizationUserByUserIdAndOrganizationId(ctx context.Context, req *pb.GetOrganizationUserByUserIdAndOrganizationIdRequest) (*pb.GetOrganizationUserByUserIdAndOrganizationIdResponse, error) {
 	return &pb.GetOrganizationUserByUserIdAndOrganizationIdResponse{}, nil
 }
 
-func (s *UserService) CreateOrganizationUser(ctx context.Context, req *pb.CreateOrganizationUserRequest) (*pb.CreateOrganizationUserResponse, error) {
-	return &pb.CreateOrganizationUserResponse{}, nil
+func (s *UserService) CreateOrganizationUser(ctx context.Context, req *pb.CreateOrganizationUserRequest) (*emptypb.Empty, error) {
+	return &emptypb.Empty{}, nil
 }
 
-func (s *UserService) DeleteOrganizationUserByUserId(ctx context.Context, req *pb.DeleteOrganizationUserByUserIdRequest) (*pb.DeleteOrganizationUserByUserIdResponse, error) {
-	return &pb.DeleteOrganizationUserByUserIdResponse{}, nil
+func (s *UserService) DeleteOrganizationUserByUserId(ctx context.Context, req *pb.DeleteOrganizationUserByUserIdRequest) (*emptypb.Empty, error) {
+	return &emptypb.Empty{}, nil
 }
 
-func (s *UserService) DeleteOrganizationUserByOrganizationId(ctx context.Context, req *pb.DeleteOrganizationUserByOrganizationIdRequest) (*pb.DeleteOrganizationUserByOrganizationIdResponse, error) {
-	return &pb.DeleteOrganizationUserByOrganizationIdResponse{}, nil
+func (s *UserService) DeleteOrganizationUserByOrganizationId(ctx context.Context, req *pb.DeleteOrganizationUserByOrganizationIdRequest) (*emptypb.Empty, error) {
+	return &emptypb.Empty{}, nil
 }
