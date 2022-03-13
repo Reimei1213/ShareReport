988f447cbb9b7d75c814a4c7373a2309b1d6bdfa
reimei <34594195+Reimei1213@users.noreply.github.com>
2022-03-13 13:40:19 +0900

    [add] #68 ログを追加

--- backend/user/usecase/organization.go
+++ backend/user/usecase/organization.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"context"
+	"log"
 	pb "share-report/proto/user"
 	"share-report/user/entity"
 
@@ -13,9 +14,11 @@ import (
 func (s *UserService) GetOrganizationById(ctx context.Context, req *pb.GetOrganizationByIdRequest) (*pb.GetOrganizationByIdResponse, error) {
 	organization, err := s.dh.GetOrganizationByID(req.Id)
 	if err != nil {
+		log.Fatal(err.Error())
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
+	log.Print("get organization by id")
 	return &pb.GetOrganizationByIdResponse{
 		Organization: s.oph.Organization(organization),
 	}, nil
@@ -24,9 +27,11 @@ func (s *UserService) GetOrganizationById(ctx context.Context, req *pb.GetOrgani
 func (s *UserService) GetOrganizationListByUserId(ctx context.Context, req *pb.GetOrganizationListByUserIdRequest) (*pb.GetOrganizationListByUserIdResponse, error) {
 	organizations, err := s.dh.GetOrganizationListByUserId(req.UserId)
 	if err != nil {
+		log.Fatal(err.Error())
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
+	log.Print("get organization list by user id")
 	return &pb.GetOrganizationListByUserIdResponse{
 		Organizations: s.oph.Organizations(organizations),
 	}, nil
@@ -40,24 +45,32 @@ func (s *UserService) CreateOrUpdateOrganization(ctx context.Context, req *pb.Cr
 	}
 	_, err := s.dh.GetOrganizationByID(organization.ID)
 	if err != nil && err != entity.ErrOrganizationNotExist {
+		log.Fatal(err.Error())
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
+	message := ""
 	if err == entity.ErrOrganizationNotExist {
 		err = s.dh.CreateOrganization(&organization)
+		message = "create organization"
 	} else {
 		err = s.dh.UpdateOrganization(&organization)
+		message = "update organization"
 	}
 	if err != nil {
+		log.Fatal(err.Error())
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
+	log.Print(message)
 	return &emptypb.Empty{}, nil
 }
 
 func (s *UserService) DeleteOrganizationById(ctx context.Context, req *pb.DeleteOrganizationByIdRequest) (*emptypb.Empty, error) {
 	err := s.dh.DeleteOrganizationByID(req.Id)
 	if err != nil {
+		log.Fatal(err.Error())
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
+	log.Print("delete organization by id")
 	return &emptypb.Empty{}, nil
 }
--- backend/user/usecase/user.go
+++ backend/user/usecase/user.go
@@ -2,6 +2,7 @@ package usecase
 
 import (
 	"context"
+	"log"
 
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -15,9 +16,11 @@ import (
 func (s *UserService) GetUserById(ctx context.Context, req *pb.GetUserByIdRequest) (*pb.GetUserByIdResponse, error) {
 	user, err := s.dh.GetUserByID(req.Id)
 	if err != nil {
+		log.Fatal(err.Error())
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
+	log.Print("get user by id")
 	return &pb.GetUserByIdResponse{
 		User: s.oph.User(user),
 	}, nil
@@ -26,9 +29,11 @@ func (s *UserService) GetUserById(ctx context.Context, req *pb.GetUserByIdReques
 func (s *UserService) GetUserListByOrganizationId(ctx context.Context, req *pb.GetUserListByOrganizationIdRequest) (*pb.GetUserListByOrganizationIdResponse, error) {
 	users, err := s.dh.GetUserListByOrganizationId(req.OrganizationId)
 	if err != nil {
+		log.Fatal(err.Error())
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
+	log.Print("get user list by organization id")
 	return &pb.GetUserListByOrganizationIdResponse{
 		Users: s.oph.Users(users),
 	}, nil
@@ -43,24 +48,32 @@ func (s *UserService) CreateOrUpdateUser(ctx context.Context, req *pb.CreateOrUp
 	}
 	_, err := s.dh.GetUserByID(user.ID)
 	if err != nil && err != entity.ErrUserNotExist {
+		log.Fatal(err.Error())
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
+	message := ""
 	if err == entity.ErrUserNotExist {
 		err = s.dh.CreateUser(&user)
+		message = "create user"
 	} else {
 		err = s.dh.UpdateUser(&user)
+		message = "update user"
 	}
 	if err != nil {
+		log.Fatal(err.Error())
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
+	log.Print(message)
 	return &emptypb.Empty{}, nil
 }
 
 func (s *UserService) DeleteUserById(ctx context.Context, req *pb.DeleteUserByIdRequest) (*emptypb.Empty, error) {
 	err := s.dh.DeleteUserByID(req.Id)
 	if err != nil {
+		log.Fatal(err.Error())
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
+	log.Print("delete user by id")
 	return &emptypb.Empty{}, nil
 }
